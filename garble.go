147d29a116c70725f37acf43c1389a9478488adb
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2014-01-31 19:00:26 +0100

    phrase flag

--- garble.go
+++ garble.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"crypto/rand"
+	cryptorand "crypto/rand"
 	"crypto/sha512"
+	"flag"
 	"fmt"
+	//	"math/rand"
+	"os"
+	"runtime"
 	"time"
+)
+
+var (
+	phrase = ""
 )
 
 // randomSeed produces a int64 seed based on crypto/rand and time.
@@ -12,7 +20,7 @@ func randomSeed() int64 {
 	var seed int64
 
 	urandom := make([]byte, 8)
-	rand.Reader.Read(urandom)
+	cryptorand.Reader.Read(urandom)
 
 	for key, value := range urandom {
 		seed ^= (int64(value) ^ time.Now().UTC().UnixNano()) << (uint(key) * 8)
@@ -21,11 +29,33 @@ func randomSeed() int64 {
 	return seed
 }
 
+func init() {
+	flag.StringVar(&phrase, "phrase", "", "the Garble phrase, by default random")
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if phrase == "" {
+		phrase = fmt.Sprintf("%016x", uint64(randomSeed()))
+	}
+}
+
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	args := flag.Args()
+
+	for i := range args {
+		fmt.Println(args[i], "with", phrase)
+	}
+
 	hash := sha512.New()
-	seed := []byte(fmt.Sprint(randomSeed()))
+	seed := []byte(phrase)
 	b := make([]byte, hash.Size())
-	for i := 0; i < 100000; i++ {
+	for i := 0; i < 2; i++ {
 		hash.Write(seed)
 		hash.Sum(b[:0])
 		fmt.Println(b)
